71cf9ef3fc9d8f038050f47e8d804b6ba00e463b
은별 <star1996416@naver.com>
2016-01-07 15:54:23 +0900

    Handler추가

--- app/src/main/java/com/busro/busro/SplashActivity.java
+++ app/src/main/java/com/busro/busro/SplashActivity.java
@@ -1,18 +1,30 @@
 package com.busro.busro;
 
-import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.*;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
+
+import java.util.logging.Handler;
 
 /**
  * Created by 은별 on 2016-01-07.
  */
 public class SplashActivity extends AppCompatActivity {
+    int SPLASHTIME = 2000;
     @Override
     public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
         setContentView(R.layout.activity_splash);
-    int SPLASH_TIME = 2000;
+        getSupportActionBar().hide();
+        new android.os.Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, SPLASHTIME);
 
     }
 }
