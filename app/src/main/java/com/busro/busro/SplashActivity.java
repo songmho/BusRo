025c63e67e2d80c815e106f9bc9d0d9a0d4bd5f0
은별 <star1996416@naver.com>
2016-01-07 15:19:04 +0900

    SplashActivity추가 activity_splash추가

---
+++ app/src/main/java/com/busro/busro/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.busro.busro;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 은별 on 2016-01-07.
+ */
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_splash);
+    int SPLASH_TIME = 2000;
+
+    }
+}
