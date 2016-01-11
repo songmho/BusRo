9c774656bc9ad0dd0db55a0f139a53eacd706595
songmho <songmho@gmail.com>
2016-01-12 02:05:39 +0900

    class들 생성. crashlytics연결.

---
+++ app/src/main/java/com/busro/busro/ChooseActivity.java
@@ -0,0 +1,17 @@
+package com.busro.busro;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by songmho on 2016-01-12.
+ */
+public class ChooseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose);
+    }
+}
--- app/src/main/java/com/busro/busro/SplashActivity.java
+++ app/src/main/java/com/busro/busro/SplashActivity.java
@@ -3,6 +3,8 @@ package com.busro.busro;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.*;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by 은별 on 2016-01-07.
@@ -12,6 +14,7 @@ public class SplashActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_splash);
         new android.os.Handler().postDelayed(new Runnable() {
             @Override
