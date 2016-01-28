23a2ae2dc4f677bcb4d5d7ef9caa2a8e3a3d45b4
songmho <songmho@gmail.com>
2016-01-28 18:31:07 +0900

    출발지,도착지 선택 시 액티비티 전환되도록 변경

--- app/src/main/java/com/busro/busro/ChooseActivity.java
+++ app/src/main/java/com/busro/busro/ChooseActivity.java
@@ -3,6 +3,7 @@ package com.busro.busro;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 /**
  * Created by songmho on 2016-01-12.
@@ -13,5 +14,13 @@ public class ChooseActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose);
+
+        Toolbar toolbar=(Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        if(getIntent().getAction()=="android.action.depart")
+            getSupportActionBar().setTitle("출발지 선택");
+        else
+            getSupportActionBar().setTitle("도착지 선택");
     }
 }
--- app/src/main/java/com/busro/busro/Dep_arr_Activity.java
+++ app/src/main/java/com/busro/busro/Dep_arr_Activity.java
@@ -1,14 +1,20 @@
 package com.busro.busro;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
 
 /**
  * Created by songmho on 2016-01-12.
  */
-public class Dep_arr_Activity extends AppCompatActivity {
+public class Dep_arr_Activity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView depart;
+    private TextView arrive;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -21,5 +27,25 @@ public class Dep_arr_Activity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("노선조회");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        depart=(TextView)findViewById(R.id.depart);
+        arrive=(TextView)findViewById(R.id.arrive);
+
+        depart.setOnClickListener(this);
+        arrive.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent=new Intent(Dep_arr_Activity.this,ChooseActivity.class);
+        switch (v.getId()){
+            case R.id.depart:
+               intent.setAction("android.action.depart");
+                break;
+            case R.id.arrive:
+                intent.setAction("android.action.arrive");
+                break;
+        }
+        startActivity(intent);
     }
 }
