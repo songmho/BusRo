e1bf8dc7e8ec797d5e63471071905d1e98016878
choijunghwan <vkdlxj3562@naver.com>
2016-02-26 02:59:40 +0900

    edittext 생성

--- app/src/main/java/com/busro/busro/BoardwriteActivity.java
+++ app/src/main/java/com/busro/busro/BoardwriteActivity.java
@@ -3,6 +3,8 @@ package com.busro.busro;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 /**
  * Created by 최정환 on 2016-02-16.
@@ -13,5 +15,10 @@ public class BoardwriteActivity extends AppCompatActivity {
         super.onCreate(saveInstancestate);
         setContentView(R.layout.board_write);
         Intent intent=new Intent(this.getIntent());
+
+        final EditText editText=(EditText)findViewById(R.id.edittext);
+        editText.getText().toString();
+
     }
+
 }
